dc45214fc4a280898f099c9fbb20674abdd2257e
Evan Hosseini <hosse005@umn.edu>
2016-05-28 18:20:07 -0500

    Add device event registration

--- server/ParticleController.js
+++ server/ParticleController.js
@@ -4,31 +4,57 @@ var fs = require('fs');
 var Particle = require('particle-api-js');
 
 var particle = new Particle();
-var cfgFile = './particle-login.json';
+var cfgFile = __dirname + '/particle-login.json';
 
 module.exports = class ParticleController {
     constructor() {
 	
 	var cfg = JSON.parse(fs.readFileSync(cfgFile, 'utf8'));
-	this.user = cfg.username;
-	this.pw = cfg.password;
 
-	// Attempt login with given username and pw and store access token
-	particle.login({username: this.user,
-			password: this.pw}).then(
-			    function (data) {
-				console.log('API call completed on promise resolve: ', data.body.access_token);
-				console.log(data);
+	// Attempt login with given username and pw
+	particle.login({username: cfg.username, password: cfg.password}).then(
+	    function (data) {
+		// Extract the access token
+		var token = data.body.access_token;
+		particle.listDevices({auth: token}).then(
+		    function(devices) {
+			// Extract the device id - just grab the first one for now..
+			var devId = devices.body[0].id;
+
+			// Now open up the event streams were interested in
+			particle.getEventStream({deviceId: devId, auth: token}).then(
+			    function(stream) {
+				stream.on('moisture', function(data) {
+				    var moisture = data.data;
+				    var timestamp = data.published_at;
+				    console.log('Moisture event: ' + data.data + ' @ ' + data.published_at);
+				    // TODO - write the data to file
+				});
+
+				stream.on('charge', function(data) {
+				    var charge = data.data;
+				    var timestamp = data.published_at;
+				    console.log('Charge event: ' + data.data + ' @ ' + data.published_at);
+				    // TODO - write the data to file
+				});
 			    },
-			    function (err) {
-				console.log('Particle API login credentials failed');
-				console.log('Check contents of the particle-login.json file\n\n');
+			    function(err) {
+				console.log('Failed to get the event stream');
 				console.log(err);
 				process.exit(0);
-			    }
-			);
-	
+			    });
+		    },
+		    function(err) {
+			console.log('Failed to query devices for the given account!');
+			console.log(err);
+			process.exit(0);
+		    });
+	    },
+	    function (err) {
+		console.log('Particle login failed, check particle-login.json\n\n');
+		console.log(err);
+		process.exit(0);
+	    });
     }
 
-    
 }
--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ var server = app.listen(8080, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Server listening at http://%s:%s', host, port);
 });
